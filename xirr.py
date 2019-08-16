45f4bb97df7c59527d426dfca3c072730360d07f
Dmitry Veber <dmitry11374@yahoo.com>
2019-08-15 23:31:57 -0500

    gradient descent repels from origin

--- xirr.py
+++ xirr.py
@@ -12,31 +12,58 @@ def transform_cashflow(time, flow):
 
 l = [
     # [-100, 75, 50, 0, 0],
-    [+100, +200, -100, -240, +50],
+    # [+100, +200, -100, -240, +50],
     # [+100, +200, -100, -250, +50],
     # [+100, +200, -100, -400, +50],
     # [+100, +200, -100, -600, +50],
+    #[-193307, 1528415]
+    [-1, -1, -1, 2.9, -1]
 ]
 
 def f_npv(cf, irr):
     return sum([cf[i] / (1 + irr)**i for i in range(len(cf))])
 
-def gradient_descent(cf, max_iter=100000000):
+def gradient_descent(cf, max_iter=10000000000):
     iter = 0
+    cf_sum = sum(cf)
+    repel_from_origin = True
     done = False
     irr = 0
-    step = 0.000001
+    step = 0.00001
     while iter < max_iter and not done:
         npv = f_npv(cf, irr)
         npv_step = f_npv(cf, irr+step)
+        print("IRR: "+str(irr) + ", NPV = " + str(npv))
+        #print(" - "+str(iter) + ": " + str(npv) + " ... " + str(npv_step))
         if npv == 0:
             return irr
-        if abs(npv) < abs(npv_step):
-            irr -= step * abs(npv)
+
+        # if going in the correct direction, deactivate repelling force
+        if abs(npv) < abs(npv_step) and cf_sum < 0:
+            repel_from_origin = False
+        if abs(npv) > abs(npv_step) and cf_sum > 0:
+            repel_from_origin = False
+
+        if repel_from_origin:
+            # fly away from origin until peak is reached
+            if cf_sum > 0:
+                irr += step * abs(npv)
+            elif cf_sum < 0:
+                irr -= step * abs(npv)
+            else:
+                return 0.0
         else:
-            irr += step * abs(npv)
+            # do general gradient descent
+            if abs(npv) < abs(npv_step):
+                irr -= step * min(abs(npv), 100)
+            else:
+                irr += step * min(abs(npv), 100)
+
+        if irr <= -1:
+            return -1
+
         iter += 1
-        print(str(irr) + ", NPV = "+str(npv))
+
     print("cannot find npv = 0")
     return irr
 
@@ -60,26 +87,27 @@ def irr(values):
     rate = rate.item(np.argmin(np.abs(rate)))
     return rate
 
-def our_calc():
-
-    for i in l:
-        result = transform_cashflow([1, 3, 9, 20, 30], i)
-        r = np.roots(result[1][::-1])
-        print("==============================")
-        print("r: "+str(r))
-        print("r.real: "+str(r.real))
-
-        # print("r.real True: " + str(r[True].real))
-
-        print("r.imag"+str(r.imag))
-        print("reciprocal: "+str(1/r.real - 1))
-        # print((1 / r.real - 1).)
-        #print(result[1])
-        print("IRR:"+str(irr(result[1])))
-        # print("NP IRR:"+str(np.irr(result[1])))
+# def our_calc():
+#
+#     for i in l:
+#         result = transform_cashflow([1, 3, 9, 20, 30], i)
+#         r = np.roots(result[1][::-1])
+#         print("==============================")
+#         print("r: "+str(r))
+#         print("r.real: "+str(r.real))
+#
+#         # print("r.real True: " + str(r[True].real))
+#
+#         print("r.imag"+str(r.imag))
+#         print("reciprocal: "+str(1/r.real - 1))
+#         # print((1 / r.real - 1).)
+#         #print(result[1])
+#         print("IRR:"+str(irr(result[1])))
+#         # print("NP IRR:"+str(np.irr(result[1])))
         #print
 
-print(gradient_descent(l[0]))
+nt, transformed_cf = transform_cashflow([1, 2, 3, 4, 5], l[0])
+print(gradient_descent(transformed_cf))
 
 
 
